1fba475592d587a1388e2876210cb0a346a9e740
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-07 00:10:30 +0000

    Task 0

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Creates a pandas DataFrame from a NumPy array."""
+
+
+import pandas as pd
+
+def from_numpy(array):
+    """Creates a DataFrame from a NumPy ndarray.
+
+    Columns are labeled alphabetically in uppercase.
+
+    Args:
+        array (numpy.ndarray): The array to convert.
+
+    Returns:
+        pandas.DataFrame: The newly created DataFrame.
+    """
+    cols = [chr(i) for i in range(ord("A"), ord("A") + array.shape[1])]
+    return pd.DataFrame(array, columns=cols)
