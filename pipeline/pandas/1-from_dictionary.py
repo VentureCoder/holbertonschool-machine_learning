eb3f03fa7c7e6ae21dc64496fe2aeba9ea69910d
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-07 00:20:07 +0000

    Add DataFrame creation from dictionary

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Creates a pandas DataFrame from a dictionary."""
+
+
+import pandas as pd
+
+
+# Create the DataFrame
+df = pd.DataFrame(
+    {
+        "First": [0.0, 0.5, 1.0, 1.5],
+        "Second": ["one", "two", "three", "four"],
+    },
+    index=["A", "B", "C", "D"],
+)
