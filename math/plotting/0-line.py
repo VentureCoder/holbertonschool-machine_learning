4b5b8dcfced9b0a985483c9d17242d44b41d690d
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-09 07:50:02 +0000

    Task 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Plots a line graph."""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """Plots y as a solid red line."""
+    y = np.arange(0, 11) ** 3
+    x = np.arange(0, 11)
+
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y, 'r-')
+    plt.xlim(0, 10)
+    plt.show()
