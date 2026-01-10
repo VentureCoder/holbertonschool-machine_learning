fbf9e7c6413063896b4f4019661cfd061375a8df
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-10 16:20:18 +0000

    Task 7

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Plots a scatter plot of mountain elevation."""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """Creates a scatter plot with elevation-based coloring."""
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+
+    plt.figure(figsize=(6.4, 4.8))
+    scatter = plt.scatter(x, y, c=z, cmap='viridis')
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
+    plt.title("Mountain Elevation")
+
+    cbar = plt.colorbar(scatter)
+    cbar.set_label("elevation (m)")
+
+    plt.show()
