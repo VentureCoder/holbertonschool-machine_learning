7782245f93024dfe2ff6453f96614a15c98f4539
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-10 15:45:26 +0000

    Task 4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -20,4 +20,6 @@ def frequency():
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    plt.xticks(range(0, 101, 10))
+    plt.xlim(0, 100)
     plt.show()
