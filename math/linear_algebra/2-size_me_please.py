6e160057cd98caf81bac3c6e10bcccf6a806d785
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-21 02:50:18 +0000

    Add matrix shape calculation function

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+2-size_me_please module
+Defines a function that returns the shape of a matrix.
+"""
+
+
+def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers."""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
