b7e69fe909596be1471376829ece60be22eb50e1
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-21 02:59:15 +0000

    Add array concatenation function

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+6-howdy_partner module
+Defines a function that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays and returns a new list."""
+    return arr1 + arr2
