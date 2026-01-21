a79a80b3cdb9b22aac1145f7bfc108bed293927f
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-21 09:29:54 +0000

    Add numpy shape function

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+10-ill_use_my_scale module
+Defines a function that returns the shape of a numpy array.
+"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray."""
+    return matrix.shape
