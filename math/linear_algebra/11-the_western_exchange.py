4d9b73901f0a70a8afffffff37bd56079984691e
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-01-21 09:44:35 +0000

    Add numpy transpose function

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+11-the_western_exchange module
+Defines a function that transposes a numpy array.
+"""
+
+
+def np_transpose(matrix):
+    """Returns the transpose of a numpy array."""
+    return matrix.T
