1a959b76613a615c6f82af56fd417c1bbe2a0ded
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-02-08 00:52:09 +0000

    Fix pycodestyle line length in intersection

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -13,7 +13,9 @@ def intersection(x, n, P, Pr):
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -21,7 +23,10 @@ def intersection(x, n, P, Pr):
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
 
-    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+    if (
+        not isinstance(Pr, np.ndarray) or
+        Pr.shape != P.shape
+    ):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
     if np.any(P < 0) or np.any(P > 1):
