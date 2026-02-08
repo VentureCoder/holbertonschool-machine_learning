1aebffce7cab1e9b7f5a17d39818db574919444f
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-02-08 00:54:08 +0000

    Fix pycodestyle line length in likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -14,7 +14,9 @@ def likelihood(x, n, P):
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -23,9 +25,10 @@ def likelihood(x, n, P):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if np.any(P < 0) or np.any(P > 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+        )
 
-    # Binomial coefficient (n choose x)
     coeff = np.math.factorial(n) / (
         np.math.factorial(x) * np.math.factorial(n - x)
     )
