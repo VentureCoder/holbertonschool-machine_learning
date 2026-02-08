991f6c855708a5265eb3bc82e668736eb5e7779f
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-02-08 01:10:36 +0000

    Add Normal CDF method

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,64 +1,45 @@
 #!/usr/bin/env python3
 """
-Normal distribution class
+Normal distribution
 """
 
 
 class Normal:
-    """Class representing a Normal distribution"""
+    """Normal distribution class"""
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """
-        Initializes the Normal distribution
-        """
+        """Class constructor"""
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
             self.mean = float(mean)
             self.stddev = float(stddev)
         else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-
-            n = len(data)
-            mean = sum(data) / n
-            var = sum((x - mean) ** 2 for x in data) / n
-
-            self.mean = float(mean)
-            self.stddev = float(var ** 0.5)
-
-    def pdf(self, x):
-        """
-        Calculates the PDF value for x
-        """
-        pi = 3.1415926536
-        e = 2.7182818285
-        z = (x - self.mean) / self.stddev
-        return (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (-0.5 * z ** 2)
+            if not isinstance(data, list) or len(data) < 2:
+                raise ValueError("data must be a list with multiple values")
+            self.mean = sum(data) / len(data)
+            var = 0
+            for x in data:
+                var += (x - self.mean) ** 2
+            self.stddev = (var / len(data)) ** 0.5
 
     def cdf(self, x):
         """
         Calculates the CDF value for x
         """
-        z = (x - self.mean) / (self.stddev * 2 ** 0.5)
-
-        # Approximate erf(z) using Taylor series
-        erf = 0
         pi = 3.1415926536
 
-        for n in range(10):
-            num = (-1) ** n * z ** (2 * n + 1)
-            den = (self._factorial(n) * (2 * n + 1))
-            erf += num / den
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
 
-        erf *= 2 / pi ** 0.5
+        erf = 0
+        for n in range(50):
+            # factorial
+            fact = 1
+            for i in range(1, n + 1):
+                fact *= i
+
+            erf += ((-1) ** n) * (z ** (2 * n + 1)) / (fact * (2 * n + 1))
+
+        erf *= (2 / (pi ** 0.5))
+
         return 0.5 * (1 + erf)
-
-    def _factorial(self, n):
-        """Computes factorial of n"""
-        fact = 1
-        for i in range(1, n + 1):
-            fact *= i
-        return fact
