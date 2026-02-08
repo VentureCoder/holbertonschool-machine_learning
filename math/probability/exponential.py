6f6bfcaa6f6ff09c8e61026a5c29b14a111c3184
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-02-08 01:09:43 +0000

    Add Exponential pdf method

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python3
 """
-Exponential distribution class
+Exponential distribution
 """
 
 
 class Exponential:
-    """Class representing an Exponential distribution"""
+    """Exponential distribution class"""
 
     def __init__(self, data=None, lambtha=1.):
-        """
-        Initializes the Exponential distribution
-        """
+        """Class constructor"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            mean = sum(data) / len(data)
-            self.lambtha = float(1 / mean)
+            if not isinstance(data, list) or len(data) < 2:
+                raise ValueError("data must be a list with multiple values")
+            mean = float(sum(data) / len(data))
+            self.lambtha = 1 / mean
 
     def pdf(self, x):
         """
-        Calculates the PDF value for a given time period
+        Calculates the PDF value for x
         """
         if x < 0:
             return 0
-        return self.lambtha * (2.718281828459045 ** (-self.lambtha * x))
 
-    def cdf(self, x):
-        """
-        Calculates the CDF value for a given time period
-        """
-        if x < 0:
-            return 0
-        return 1 - (2.718281828459045 ** (-self.lambtha * x))
+        e = 2.7182818285
+
+        return self.lambtha * (e ** (-self.lambtha * x))
