3cb23668a85682e32fb93377aff3416643ecc903
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-04-12 12:25:05 +0000

    Add Task

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-'''
-contrast
-'''
-
+"""
+Task 3
+"""
 import tensorflow as tf
 
 
 def change_contrast(image, lower, upper):
-    '''
-    image - 3D tf.Tensor that contains the image to adjust the contrast
-    lower - A float representing the lower bound of the random contrast factor
-    range.
-    upper - A float representing the upper bound of the random contrast factor
-    range.
-    '''
+    """
+    """
     return tf.image.random_contrast(image, lower, upper)
