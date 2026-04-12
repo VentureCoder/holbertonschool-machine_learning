4d8a4685dfaf90d2015339cf2c33d80821b4c873
Orkhan Suleymanov <osul0003@student.monash.edu>
2026-04-12 12:11:54 +0000

    Add 4-brightness.py

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-'''
-change brightness
-'''
+"""
+Task 4
+"""
 
 import tensorflow as tf
 
 
 def change_brightness(image, max_delta):
-    '''
-    image - 3D tf.Tensor containing the image to change
-    max_delta - the maximum amount of the image should be brightened
-    '''
+    """
+    """
     return tf.image.random_brightness(image, max_delta)
